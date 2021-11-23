7e8bb3c40b1a444afe57fea23f771a0f8ccfa16d
dlrlsud@naver.com <dlrlsud@naver.com>
2021-11-24 00:32:44 +0900

    1

---
+++ src/components/ListCp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListCp',
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ListWrapCp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListWrapCp',
+};
+</script>
+
+<style>
+
+</style>
